02d370c24d876672e0d8176bac4ca87aa0757ccb
VictorGabrielMO <victorgabrielengsis@gmail.com>
2025-01-26 19:21:36 +0000

    Initial commit

---
+++ exp_feat_single_gpu.py
@@ -0,0 +1,259 @@
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset
+import torchvision.transforms as transforms
+from sklearn.metrics import average_precision_score, precision_score, recall_score, accuracy_score
+import numpy as np
+from PIL import Image
+import os
+#import clip
+from tqdm import tqdm
+import timm
+import argparse
+import random
+from sklearn.metrics import accuracy_score, precision_score
+import json
+import torchvision.models as vis_models
+from pathlib import Path
+
+from dataset import *
+#from augment import ImageAugmentor
+from mask import *
+#from utils import *
+from extract_single_gpu import *
+
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
+from torch.utils.data.distributed import DistributedSampler
+
+os.environ['NCCL_BLOCKING_WAIT'] = '1'
+os.environ['NCCL_DEBUG'] = 'WARN'
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Settings for your script")
+
+    parser.add_argument(
+        '--model_name',
+        default='RN50',
+        type=str,
+        choices=[
+            'RN18', 'RN34', 'RN50', 'RN50_mod', 'clip_rn50', 'clip_vitl14',
+        ],
+        help='Type of model to use; includes ResNet variants'
+        )
+    parser.add_argument(
+        '--clip_ft', 
+        action='store_true', 
+        help='For loading a finetuned clip model'
+        )
+    parser.add_argument(
+        '--mask_type', 
+        default='spectral', 
+        choices=[
+            'patch', 
+            'spectral',
+            'pixel', 
+            'nomask'], 
+        help='Type of mask generator'
+        )
+    parser.add_argument(
+        '--band', 
+        default='all',
+        type=str,
+        choices=[
+            'all', 'low', 'mid', 'high',]
+        )
+    parser.add_argument(
+        '--pretrained', 
+        action='store_true', 
+        help='For pretraining'
+        )
+    parser.add_argument(
+        '--ratio', 
+        type=int, 
+        default=50,
+        help='Ratio of mask to apply'
+        )
+    parser.add_argument(
+        '--batch_size', 
+        type=int, 
+        default=64, 
+        help='Batch Size'
+        )
+    parser.add_argument(
+        '--data_type', 
+        default="Wang_CVPR20", 
+        type=str, 
+        choices=['Wang_CVPR20', 'Ojha_CVPR23','ArtiFact'], 
+        help="Dataset Type"
+        )
+    parser.add_argument(
+        '--other_model', 
+        action='store_true', 
+        help='if the model is from my own code'
+        )
+    #parser.add_argument("--local_rank", type=int)
+    #parser.add_argument("--local-rank","--local_rank", type=int)
+    #parser.add_argument("--local-rank", "--local_rank")
+    parser.add_argument('--checkpoint_path', type=Path, help='Path for the weights')
+    parser.add_argument('--save_dir', type=Path, help='Save dir for the results')
+
+    args = parser.parse_args()
+
+
+    # Set the device to CUDA
+    #device = torch.device("cuda")
+    #print(f"Using device: {device}")
+    
+    seed = 42
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+    #local_rank = int(os.environ["LOCAL_RANK"])
+    #print("aqui")
+    #print(local_rank)
+
+    
+    #device = torch.device(f'cuda:{local_rank}')
+    #torch.cuda.set_device(local_rank)
+    if not torch.cuda.is_available():
+        raise Exception("CUDA not available")
+    
+    device = torch.device("cuda")
+    
+    #dist.init_process_group(backend='nccl')
+
+    model_name = args.model_name.lower()
+    finetune = 'ft' if args.pretrained else ''
+    band = '' if args.band == 'all' else args.band
+    
+    ratio = args.ratio
+
+    #CHANGED
+    checkpoint_path = "/storage/datasets/gabriela.barreto/rn50_modft_spectralmask.pth"
+
+    # Define the path to the results file
+    results_dir = args.save_dir
+    #os.makedirs(results_dir,exist_ok=True)
+    #fake_results_dir = os.path.join(results_dir, "/fake")
+    #os.makedirs(fake_results_dir, exist_ok=True)
+    #real_results_dir = os.path.join(results_dir, "/real")
+    #os.makedirs(real_results_dir, exist_ok=True)
+
+    # Pretty print the arguments
+    print("\nSelected Configuration:")
+    print("-" * 30)
+    print(f"Device: {local_rank}")
+    print(f"Dataset Type: {args.data_type}")
+    print(f"Model type: {args.model_name}")
+    print(f"Ratio of mask: {ratio}")
+    print(f"Batch Size: {args.batch_size}")
+    print(f"Mask Type: {args.mask_type}")
+    print(f"Checkpoint Type: {checkpoint_path}")
+    print(f"Results saved to: {results_dir}")
+    print("-" * 30, "\n")
+
+    if args.data_type == 'Wang_CVPR20':
+        datasets = {
+            'ProGAN': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/progan',
+            'CycleGAN': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/cyclegan',
+            'BigGAN': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/biggan',
+            'StyleGAN': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/stylegan',
+            'GauGAN': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/gaugan',
+            'StarGAN': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/stargan',
+            'DeepFake': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/deepfake',
+            'SITD': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/seeingdark',
+            'SAN': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/san',
+            'CRN': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/crn',
+            'IMLE': '/home/users/chandler_doloriel/scratch/Datasets/Wang_CVPR2020/testing/imle',
+        }
+    # elif args.data_type == 'GenImage':
+    #     datasets = {
+    #         'VQDM': '/home/users/chandler_doloriel/scratch/Datasets/GenImage/imagenet_vqdm/imagenet_vqdm/val',
+    #         'Glide': '/home/users/chandler_doloriel/scratch/Datasets/GenImage/imagenet_glide/imagenet_glide/val',
+    #     }
+    elif args.data_type == 'Ojha_CVPR23':
+        datasets = {
+            'Guided': '/home/users/chandler_doloriel/scratch/Datasets/Ojha_CVPR2023/guided',
+            'LDM_200': '/home/users/chandler_doloriel/scratch/Datasets/Ojha_CVPR2023/ldm_200',
+            'LDM_200_cfg': '/home/users/chandler_doloriel/scratch/Datasets/Ojha_CVPR2023/ldm_200_cfg',
+            'LDM_100': '/home/users/chandler_doloriel/scratch/Datasets/Ojha_CVPR2023/ldm_100',
+            'Glide_100_27': '/home/users/chandler_doloriel/scratch/Datasets/Ojha_CVPR2023/glide_100_27',
+            'Glide_50_27': '/home/users/chandler_doloriel/scratch/Datasets/Ojha_CVPR2023/glide_50_27',
+            'Glide_100_10': '/home/users/chandler_doloriel/scratch/Datasets/Ojha_CVPR2023/glide_100_10',
+            'DALL-E': '/home/users/chandler_doloriel/scratch/Datasets/Ojha_CVPR2023/dalle',       
+        }
+    elif args.data_type == 'ArtiFact':
+        real_datasets = {
+            "afhq": "/storage/datasets/gabriela.barreto/artifact/afhq",
+            "celebAHQ": "/storage/datasets/gabriela.barreto/artifact/celebahq",
+            "coco": "/storage/datasets/gabriela.barreto/artifact/coco",
+            "ffhq": "/storage/datasets/gabriela.barreto/artifact/ffhq",
+            "imagenet": "/storage/datasets/gabriela.barreto/artifact/imagenet",
+            "landscape": "/storage/datasets/gabriela.barreto/artifact/landscape",
+            "lsun": "/storage/datasets/gabriela.barreto/artifact/lsun",
+            "metfaces": "/storage/datasets/gabriela.barreto/artifact/metfaces",
+            "cycle_gan": "/storage/datasets/gabriela.barreto/artifact/cycle_gan"
+        }
+
+
+
+            
+        fake_datasets = {
+            "big_gan": "/storage/datasets/gabriela.barreto/artifact/big_gan"
+        }
+
+    else:
+        raise ValueError("wrong dataset type")
+
+
+    
+    
+    #all_fake_features = np.empty((0, feature_dim))
+    #all_real_features = np.empty((0, feature_dim))
+    
+    for dataset_name, dataset_path in real_datasets.items():
+        
+        prediction_real, features_real, _ = extract_evaluation_features(
+            args.model_name,
+            args.data_type,
+            args.mask_type,
+            ratio/100,
+            dataset_path,
+            dataset_name,
+            args.batch_size,
+            checkpoint_path,
+            device,
+            args,
+            label=0
+        )
+
+        #features_real = features_real.numpy()
+        np.savez(os.path.join(results_dir,f"real_{dataset_name}.npz"), y_hat=prediction_real ,feat=features_real )
+        
+        
+    for dataset_name, dataset_path in fake_datasets.items():
+        prediction_fake, features_fake, _ = extract_evaluation_features(
+            args.model_name,
+            args.data_type,
+            args.mask_type,
+            ratio/100,
+            dataset_path,
+            dataset_name,
+            args.batch_size,
+            checkpoint_path,
+            device,
+            args,
+            label=0
+        )
+
+        #features_fake = features_fake.numpy()
+        np.savez(os.path.join(results_dir,f"fake_{dataset_name}.npz"), y_hat = prediction_fake, feat=features_fake )
+    
+   # np.savez( os.path.join(results_dir,"all_features.npz"), fake=all_fake_features, real = all_real_features )
+    
---
+++ extract_single_gpu.py
@@ -0,0 +1,131 @@
+import torch
+import os
+import torch.nn as nn
+import numpy as np
+from tqdm import tqdm
+from sklearn.metrics import accuracy_score, average_precision_score, roc_auc_score
+#import torch.distributed as dist  # If distributed training is being used
+import wandb  # If Weights & Biases is being used for logging
+
+from torchvision import transforms
+from torch.utils.data import DataLoader, TensorDataset
+import timm 
+
+import torchvision.models as vis_models
+
+from dataset import *
+#from augment import ImageAugmentor
+from mask import *
+#from utils import *
+from networks.resnet import resnet50
+from networks.resnet_mod import resnet50 as _resnet50, ChannelLinear
+
+#from networks.clip_models import CLIPModel
+import time
+
+#from torch.nn.parallel import DistributedDataParallel
+#from torch.utils.data.distributed import DistributedSampler
+
+#os.environ['NCCL_BLOCKING_WAIT'] = '1'
+#os.environ['NCCL_DEBUG'] = 'WARN'
+
+def extract_evaluation_features(
+    model_name,
+    data_type,
+    mask_type, 
+    ratio,
+    dataset_path,
+    dataset_name,
+    batch_size,
+    checkpoint_path, 
+    device,
+    args,
+    label=0
+    ):
+
+    # Depending on the mask_type, create the appropriate mask generator
+    if mask_type == 'spectral':
+        mask_generator = FrequencyMaskGenerator(ratio=ratio)
+    else:
+        mask_generator = None
+
+
+    #test_opt = {
+    #    'rz_interp': ['bilinear'],
+    #    'loadSize': 256,
+    #    'blur_prob': 0.1,  # Set your value
+    #    'blur_sig': [(0.0 + 3.0) / 2],
+    #    'jpg_prob': 0.1,  # Set your value
+    #    'jpg_method': ['pil'],
+    #    'jpg_qual': [int((30 + 100) / 2)]
+    #}
+
+    #test_transform = test_augment(ImageAugmentor(test_opt), mask_generator, args)
+  
+
+    if data_type == 'ArtiFact':
+        test_dataset = ArtiFact(dataset_path, label)
+    else:
+        raise ValueError("wrong dataset input")
+
+    #test_sampler = DistributedSampler(test_dataset)
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+
+
+    if model_name == 'RN50_mod':
+        model = _resnet50(pretrained=False, stride0=1)
+        model.fc = ChannelLinear(model.fc.in_features, 1)
+    else:
+        raise ValueError(f"Model {model_name} not recognized!")
+
+    model = model.to(device)
+    #model = DistributedDataParallel(model, find_unused_parameters=True)
+    checkpoint = torch.load(checkpoint_path, map_location='cpu')['model_state_dict']
+
+    # Check if model was trained with DataParallel or DistributedDataParallel
+    if 'module.' in list(checkpoint.keys())[0]:  # Check if the weights are wrapped in 'module.'
+        # Create a new dictionary without the 'module.' prefix
+        new_checkpoint = {}
+        for k, v in checkpoint.items():
+            new_checkpoint[k.replace('module.', '')] = v
+        checkpoint = new_checkpoint
+
+    # Load the weights into the model
+    model.load_state_dict(checkpoint)
+
+    model = model.to(device)
+    model.eval() 
+    
+    features = np.empty((0, 2048))
+    pred_list = []
+    labels_list = []
+    
+
+    #disable_tqdm = dist.get_rank() != 0
+    #data_loader_with_tqdm = tqdm(test_dataloader, "test dataloading", disable=disable_tqdm)
+
+    print(f"### Iniciando extraçao de features no dataset {dataset_name} ...")
+    i = 0
+    num_batches = len(test_dataloader)
+    with torch.no_grad():
+        for inputs, labels in test_dataloader:
+            inputs = inputs.to(device)
+            y_pred, embeddings = model(inputs, return_feats=True)
+            embeddings = embeddings.detach().cpu()
+            embeddings = embeddings.mean(dim=(2,3)) # GAP
+            y_pred = y_pred.view(-1).unsqueeze(1)
+            features = np.concatenate((features, embeddings.numpy()), axis=0)
+            pred_list.extend(y_pred.sigmoid().detach().cpu().numpy())
+            labels_list.extend(labels.cpu().numpy())
+            if (i % 100 == 0):
+                print(f"Numero de batches computados: {i} / {num_batches}")
+            
+            i += 1
+
+    #features = torch.cat(features)
+    labels_list, pred_list = np.array(labels_list), np.array(pred_list)
+    labels_list[labels_list > 1] = 1
+    
+    #labels_list = torch.cat(labels_list).numpy()
+
+    return pred_list, features, labels_list
